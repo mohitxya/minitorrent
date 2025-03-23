f19f90209d7ae4664818ba7680f42418e7104c13
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-03-23 14:15:37 +0530

    add a few files

--- minitorrent/parser.py
+++ minitorrent/parser.py
@@ -7,7 +7,7 @@ import random
 
 
 
-with open("../tests/file.torrent","rb") as f:
+with open("../tests/file3.torrent","rb") as f:
     data=bencoder.decode(f.read())
     od=OrderedDict(data)
     info_dict=od[b'info']
@@ -18,10 +18,12 @@ with open("../tests/file.torrent","rb") as f:
     info_bencoded=bencoder.encode(info_dict)
     info_hash_b=hashlib.sha1(info_bencoded).digest()
 
-    url_encoded_info_hash = ''.join(f'%{b:02X}' for b in info_hash_b)
-    print(url_encoded_info_hash)
+    info_hash = ''.join(f'%{b:02X}' for b in info_hash_b)
+    print(info_hash)
+
 
 
 peer_id='-PC0001-' + ''.join([str(random.randint(0, 9)) for _ in range(12)])
 
-
+print(peer_id)
+pprint(od)
